c58a053860bb4d2b832f59ae4e30c51a67eedea8
nnoopt <nnoopt@gmail.con>
2021-12-09 19:53:55 +0300

    work

--- src/main/java/nnoopt/client/ChatApp.java
+++ src/main/java/nnoopt/client/ChatApp.java
@@ -6,10 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class ChatApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("chat.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("cloud.fxml"));
         primaryStage.setScene(new Scene(parent));
         primaryStage.show();
     }
--- src/main/java/nnoopt/client/OnReceived.java
+++ src/main/java/nnoopt/client/OnReceived.java
@@ -2,6 +2,7 @@ package nnoopt.client;
 
 import nnoopt.AbstractMessage;
 
+
 public interface OnReceived {
     void onReceive(AbstractMessage msg);
 }
